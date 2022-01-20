eb07ba2bf8f1f5a6cf40e1f7a65f1b6602f41f81
taka01150810 <taka01150810@gmail.com>
2022-01-20 17:29:48 +0900

    属性で設定。tailwindcssga

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">
         ヘッダー1です。
     </x-slot>
-    コンポーネントテスト2(検証すると違いがわかる)
+    コンポーネントテスト1(検証すると違いがわかる)
+    <x-tests.card title="タイトル" content="本文" />
 </x-tests.app>
