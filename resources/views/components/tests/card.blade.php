f332c5b02cb18c2b7a5b2e13f31f08b5cf02d1c3
taka01150810 <taka01150810@gmail.com>
2022-01-20 17:52:11 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,8 @@
+@props([
+    'title',
+    'message' => '初期値です。',
+    'content' => '本文の初期値です。'
+])
 <div class="bg-indigo-400 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,6 @@
     コンポーネントテスト1(検証すると違いがわかる)
     <x-tests.card title="タイトル" content="本文" message="$message"/>
     <x-tests.card title="タイトル" content="本文" :message="$message"/>
+    {{-- 下記だとcontentとmessageがないのでエラーが出てしまう --}}
+    {{-- <x-tests.card title="タイトル2"> --}}
 </x-tests.app>
