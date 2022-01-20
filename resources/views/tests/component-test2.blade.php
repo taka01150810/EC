0c65d6527369f5b4c8bd6a6a9cf02283d3c2e6aa
taka01150810 <taka01150810@gmail.com>
2022-01-20 19:00:35 +0900

    クラスベース（コンポーネント、属性、初期値)

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         ヘッダー2です。
     </x-slot>
     コンポーネントテスト2(検証すると違いがわかる)
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+        <x-test-class-base classBaseMessage="属性コンポーネントのメッセージです" defaultMessage="初期値から変更しています"/>
 </x-tests.app>
