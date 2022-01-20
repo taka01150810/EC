47b78219ec9f1f10c4eae43537bc478ce0766a96
taka01150810 <taka01150810@gmail.com>
2022-01-20 17:13:50 +0900

    名前つきスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        ヘッダー1です。
+    </x-slot>
     コンポーネントテスト2(検証すると違いがわかる)
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        ヘッダー2です。
+    </x-slot>
     コンポーネントテスト2(検証すると違いがわかる)
 </x-tests.app>
