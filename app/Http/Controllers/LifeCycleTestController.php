a7e26c7f3754d11e1f6cffc8d10701d3b8c526e9
taka01150810 <taka01150810@gmail.com>
2022-01-21 11:07:43 +0900

    サービスコンテナ2

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -15,9 +15,36 @@ class LifeCycleTestController extends Controller
         //サービスコンテナから取り出す
         $test = app()->make('lifeCycleTest');
 
+        //サービスコンテナなしのパターン
+        $message = new Message();
+        $sample = new Sample($message);
+        $sample->run();//出力結果 メッセージ表示
+
+        //サービスコンテナありのパターン
+        app()->bind('sample', Sample::class);//サークルコンテナに登録する
+        $sample = app()->make('sample');//サービスコンテナから取り出す
+        $sample->run();//出力結果 メッセージ表示
+
         dd(app(), $test);
         //app() -> 中身を確認できる。bindings: array:71 -> 71個のサービスが設定されている。
         //$test -> 出力結果 ライフサイクルテスト
 
+    }
+}
+class Sample
+{
+    public $message;
+    public function __construct(Message $message){
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send(){
+        echo('メッセージ表示');
     }
 }
