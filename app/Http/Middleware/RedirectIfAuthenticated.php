a9e23d7dd0aed919e63a04dc98c3ffbed5fd0d0e
taka01150810 <taka01150810@gmail.com>
2022-01-21 14:33:20 +0900

    マルチログイン(ログイン済みのユーザーがアクセスした時のリダイレクト処理)

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -7,8 +7,12 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+//ログイン済みのユーザーがアクセスした時のリダイレクト処理
 class RedirectIfAuthenticated
 {
+    private const GUARD_USER = 'users';
+    private const GUARD_OWNER = 'owners';
+    private const GUARD_ADMIN = 'admin';
     /**
      * Handle an incoming request.
      *
@@ -19,12 +23,25 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        // $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        // foreach ($guards as $guard) {
+        //     if (Auth::guard($guard)->check()) {
+        //         return redirect(RouteServiceProvider::HOME);
+        //     }
+        // }
+
+        //ガード設定対象のユーザーなら and 受信リクエストが名前つきルートに一致するなら
+        if(Auth::guard(self::GUARD_USER)->check() && $request->routeIs('user.*')){
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        if(Auth::guard(self::GUARD_OWNER)->check() && $request->routeIs('owner.*')){
+            return redirect(RouteServiceProvider::OWNER_HOME);
+        }
+
+        if(Auth::guard(self::GUARD_ADMIN)->check() && $request->routeIs('admin.*')){
+            return redirect(RouteServiceProvider::ADMIN_HOME);
         }
 
         return $next($request);
