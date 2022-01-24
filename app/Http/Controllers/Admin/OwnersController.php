4d4afb9a1fa764839a954a8ab6ec3a03022a3f33
taka01150810 <taka01150810@gmail.com>
2022-01-24 16:12:06 +0900

    admin.update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -96,7 +96,14 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index');
     }
 
     /**
