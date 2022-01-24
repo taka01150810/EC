d114d2888245b308a935814a646651cfcfcb52fa
taka01150810 <taka01150810@gmail.com>
2022-01-24 12:10:56 +0900

    admin.index

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -24,25 +24,10 @@ class OwnersController extends Controller
      */
     public function index()
     {
-        //カーボン
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now;//出力結果 現在の時間
-        echo $date_parse;//出力結果 現在の時間
-        //エロクアント
-        $e_all = Owner::all();//返り値はEloquentCollection
-        //クエリビルダ
-        $q_get = DB::table('owners')->select('name','created_at')->get();//返り値はCollection
-        $q_first = DB::table('owners')->select('name')->first();//返り値はstdClass
-        //コレクション
-        $c_test = collect([//返り値はCollection
-            'name' => 'テスト'
-        ]);
-
-        // dd($e_all, $q_get, $q_first, $c_test);
+        $owners = Owner::select('name', 'email', 'created_at')->get();
 
         return view('admin.owners.index',
-        compact('e_all', 'q_get'));
+        compact('owners'));
     }
 
     /**
