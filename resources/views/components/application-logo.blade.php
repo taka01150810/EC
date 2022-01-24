ceb7f9ae688d777d5e667a2abf64cd0c09b76ee8
taka01150810 <taka01150810@gmail.com>
2022-01-24 11:05:07 +0900

    アプリ名、ロゴ修正

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,6 @@
 <div class="w-8">
     {{-- publicフォルダのimagesを指定している。public/imagesには初期画像。
     storage/app/publicはアップロードされる画像などを保管する。 --}}
-    <img src={{  asset('images/logo.png') }}>
+    {{-- 本来はblade側でCSSは調整する --}}
+    <img src="{{ asset("images/logo.png") }}">
 </div>
