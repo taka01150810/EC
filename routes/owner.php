55838d095a7a2e0da0e9d7551b1a2a8f0333a46b
taka01150810 <taka01150810@gmail.com>
2022-01-25 16:04:20 +0900

    Owner(image初期設定)

--- routes/owner.php
+++ routes/owner.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +36,10 @@ Route::prefix('shops')->middleware(['auth:owners'])
     Route::get('edit/{shop}',[ShopController::class,'edit'])->name('shops.edit');
     Route::post('update/{shop}',[ShopController::class,'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')
+->except(['show']);//showは今回使わないので
 
 //auth.phpからコピー
 Route::get('/register', [RegisteredUserController::class, 'create'])
