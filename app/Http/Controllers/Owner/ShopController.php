916c57f9000d379ab4e56670177a331e622bd622
taka01150810 <taka01150810@gmail.com>
2022-01-25 13:14:27 +0900

    Owner(Shop 画像アップロード(リサイズなし))

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -39,11 +40,18 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
+        //リサイズしないパターン
+        $imageFile = $request->image; //一時保存
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            Storage::putFile('public/shops', $imageFile);//putFileでファイル名を生成。サーバー側に保存。
+        }
 
+        return redirect()->route('owner.shops.index');
     }
 }
