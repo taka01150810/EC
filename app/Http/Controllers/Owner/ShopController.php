391691cad642cf4cb9e635e47f42e32a8d3b9ec0
taka01150810 <taka01150810@gmail.com>
2022-01-25 12:16:52 +0900

    Owners(Shop コントローラミドルウェア)

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -13,6 +13,21 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+        // owner/shops/edit/2/で数字を変更すると
+        // 他のオーナーのShopが見れてしまうので
+        // ログイン済みのオーナーのShop URLでなければ404画面を表示
+        $this->middleware(function($request, $next){
+            $id = $request->route()->parameter('shop'); //shopのid取得
+            if(!is_null($id)){ //null判定
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
+                $ownerId = Auth::id();
+            if($shopId !== $ownerId){ // 同じでなかったら
+                abort(404); // 404画面表示
+            }
+        }
+            return $next($request);
+        });
     }
 
     public function index()
