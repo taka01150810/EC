c247329810b5180e4a190f43a750d739072ce3c0
taka01150810 <taka01150810@gmail.com>
2022-01-25 15:42:54 +0900

    Owner(Shop edit update残り)

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -56,6 +56,27 @@ class ShopController extends Controller
             $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
-        return redirect()->route('owner.shops.index');
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'is_selling' => 'required',
+        ]);
+
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $shop->filename = $fileNameToStore;
+        }
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with([
+            'message' => '店舗情報を更新しました',
+            'status'=>'info',
+        ]);
     }
 }
--- resources/views/components/shop-thumbnail.blade.php
+++ resources/views/components/shop-thumbnail.blade.php
@@ -1,7 +1,7 @@
-@if(empty($shop->filename))
+@if(empty($filename))
 {{-- 初期設定の画像は publicフォルダに入る --}}
 <img src="{{ asset('images/no_image.jpg')}}">
 @else
 {{-- アップロードされる画像は storageフォルダに入る --}}
-<img src="{{ asset('storage/shops/' . $shop->filename) }}">
+<img src="{{ asset('storage/shops/' . $filename) }}">
 @endif
