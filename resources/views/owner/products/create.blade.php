43650543ac7fa9fe071020c0c832b9b7429426c8
taka01150810 <taka01150810@gmail.com>
2022-01-26 15:35:45 +0900

    Owner(Micromodal.jsその2)

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -27,7 +27,7 @@
                                    </select>
                                 </div>
                             </div>
-                            <x-select-image name="image1" />
+                            <x-select-image :images="$images" name="image1"></x-select-image>
                             <div class="p-4 w-full flex justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index')}}'" class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">登録する</button>
@@ -38,4 +38,21 @@
             </div>
         </div>
     </div>
+<script>
+'use strict'
+ const images = document.querySelectorAll('.image') //全てのimageタグを取得
+ images.forEach(image => { // 1つずつ繰り返す
+    image.addEventListener('click', function(e){ // クリックしたら
+        const imageName = e.target.dataset.id.substr(0, 6) //data-idの6文字
+        const imageId = e.target.dataset.id.replace(imageName + '_', '') // 6文字カット
+        const imageFile = e.target.dataset.file
+        const imagePath = e.target.dataset.path
+        const modal = e.target.dataset.modal
+        // サムネイルと input type=hiddenのvalueに設定
+        document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+        document.getElementById(imageName + '_hidden').value = imageId
+        MicroModal.close(modal); //モーダルを閉じる
+    })
+ })
+</script>
 </x-app-layout>
