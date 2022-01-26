a48b0f08bef3a37fddef1424bc8aeb8d5ff023e4
taka01150810 <taka01150810@gmail.com>
2022-01-26 10:35:18 +0900

    Owner(Image ダミーデータ)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class, //外部キー制約がある場合は事前に必要なデータ(Owner)を設定する
+            ImageSeeder::class,
         ]);
     }
 }
