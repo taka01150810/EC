22e713be646e7d4eec40c3ae2fba89dedd08a756
taka01150810 <taka01150810@gmail.com>
2022-01-31 11:17:04 +0900

    Cartの保存処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function add(Request $request)
     {
-        dd($request);
+        $itemInCart = Cart::where('user_id', Auth::id())// ログインしているユーザーか
+        ->where('product_id', $request->product_id)->first(); //カートに商品があるか確認
+        if($itemInCart){
+            $itemInCart->quantity += $request->quantity; //あれば数量を追加
+            $itemInCart->save();
+        } else {
+            Cart::create([ // なければ新規作成
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity
+            ]);
+        }
+        return redirect()->route('user.cart.index');
     }
 }
